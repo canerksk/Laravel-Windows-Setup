1a5f040f4f7d2ff0ff64849efda5b1be89740641
Caner <caner@webtein.com>
2023-04-19 02:21:21 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Laravel_Setup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.projectname_textbox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.installdir_textbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buildButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Laravel Sürümü";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Proje Adı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kurulacak Dizin";
+            // 
+            // projectname_textbox
+            // 
+            this.projectname_textbox.Location = new System.Drawing.Point(117, 17);
+            this.projectname_textbox.Name = "projectname_textbox";
+            this.projectname_textbox.Size = new System.Drawing.Size(217, 20);
+            this.projectname_textbox.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(11, 106);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(322, 199);
+            this.listBox1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(117, 77);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(217, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // installdir_textbox
+            // 
+            this.installdir_textbox.Location = new System.Drawing.Point(117, 51);
+            this.installdir_textbox.Name = "installdir_textbox";
+            this.installdir_textbox.Size = new System.Drawing.Size(189, 20);
+            this.installdir_textbox.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // buildButton
+            // 
+            this.buildButton.Location = new System.Drawing.Point(11, 314);
+            this.buildButton.Name = "buildButton";
+            this.buildButton.Size = new System.Drawing.Size(75, 23);
+            this.buildButton.TabIndex = 8;
+            this.buildButton.Text = "Kur";
+            this.buildButton.UseVisualStyleBackColor = true;
+            this.buildButton.Click += new System.EventHandler(this.buildButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 349);
+            this.Controls.Add(this.buildButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.installdir_textbox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.projectname_textbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Laravel Windows Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox projectname_textbox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox installdir_textbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buildButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace Laravel_Setup
+{
+    public partial class Form1 : Form
+    {
+
+        private static StringBuilder output = new StringBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+            installdir_textbox.Text = Application.StartupPath;
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("laravel", "1"));
+
+            var repo = "laravel/laravel";
+            var contentsUrl = $"https://api.github.com/repos/{repo}/releases?per_page=100";
+            var contentsJson = await httpClient.GetStringAsync(contentsUrl);
+            var contents = (JArray)JsonConvert.DeserializeObject(contentsJson);
+
+            foreach (var file in contents)
+            {
+                var fileType = (string)file["type"];
+
+                var TagName = file["tag_name"];
+                string VersionName = TagName.ToString().Substring(1, TagName.ToString().Length - 1);
+
+                //Console.WriteLine(file["tag_name"]);
+                comboBox1.Items.Add(VersionName);
+                //if (fileType == "dir")
+                //{
+                // var directoryContentsUrl = (string)file["url"];
+                // Console.WriteLine($"DIR: {directoryContentsUrl}");
+                //}
+                // else if (fileType == "file")
+                //{
+                //var downloadUrl = (string)file["download_url"];
+                // Console.WriteLine($"DOWNLOAD: {downloadUrl}");
+                //}
+            }
+
+            //projectname_textbox.Text = "";
+
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+
+            bool InstalledPhp = false;
+            bool InstalledComposer = false;
+            try
+            {
+                //\wamp64\bin\php\php8.2.1; 
+                //\wamp64\composer;
+                //\wamp64\bin\php\php8.2.1;
+                //\wamp64\bin\php\Composer;
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment"))
+                {
+                    if (key != null)
+                    {
+                        Object o = key.GetValue("Path");
+                        if (o != null)
+                        {
+                            string[] RegValues = o.ToString().Split(';');
+                            foreach (var RegValue in RegValues)
+                            {
+                                if (RegValue.Contains("php"))
+                                {
+                                    InstalledPhp = true;
+                                }
+                                if (RegValue.Contains("composer"))
+                                {
+                                    InstalledComposer = true;
+                                }
+                                //Console.WriteLine(RegValue);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (InstalledPhp != true)
+            {
+                buildButton.Enabled = false;
+                listBox1.Items.Add("Sisteminizde kurulu PHP bulunamadı");
+                return;
+            }
+
+            if (InstalledComposer != true)
+            {
+                buildButton.Enabled = false;
+                listBox1.Items.Add("Sisteminizde kurulu Composer bulunamadı");
+                return;
+            }
+
+        }
+
+        private void buildButton_Click(object sender, EventArgs e)
+        {
+            string projectnameDirectroyName = projectname_textbox.Text;
+
+            if (string.IsNullOrEmpty(projectnameDirectroyName))
+            {
+                MessageBox.Show("Proje adı giriniz.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                //listBox1.Items.Add("Proje adı giriniz.");
+                return;
+            }
+            if (Directory.Exists(projectnameDirectroyName))
+            {
+                MessageBox.Show("Bu isimde bir proje bulunuyor.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                //listBox1.Items.Add("Bu isimde bir proje bulunuyor.");
+                return;
+            }
+
+            listBox1.Items.Add("Laravel v." + comboBox1.Text + " " + projectname_textbox.Text + " adıyla  kuruluyor, lütfen bekleyiniz...");
+            buildButton.Enabled = false;
+
+            Process process = new Process();
+            //process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            // composer create-project laravel/laravel:^9.* project_name
+            process.StartInfo.FileName = "cmd.exe";
+            //process.StartInfo.WorkingDirectory = Application.StartupPath;
+            process.StartInfo.Arguments = "/C composer create-project laravel/laravel:^" + comboBox1.Text + " " + projectname_textbox.Text;
+            Console.WriteLine("/C composer create-project laravel/laravel:^" + comboBox1.Text + " " + projectname_textbox.Text);
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+
+            process.OutputDataReceived += new DataReceivedEventHandler((ss, ee) =>
+            {
+                if (!String.IsNullOrEmpty(ee.Data))
+                {
+                    Console.WriteLine(ee.Data);
+                }
+            });
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+            Console.WriteLine(output);
+            process.WaitForExit();
+            process.Close();
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadLine();
+            listBox1.Items.Add("Laravel " + comboBox1.Text + " " + projectname_textbox.Text + " adıyla başarıyla kuruldu...");
+            buildButton.Enabled = true;
+        }
+
+
+    }
+}
