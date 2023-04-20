e3a43ceec659283de86e7dd747855cf19082951b
Caner <gralrummy@gmail.com>
2023-04-20 12:29:27 +0300

    Assemblyinfo düzenlemesi.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Laravel-Setup")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Laravel Windows Yükleme Aracı")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Laravel-Setup")]
